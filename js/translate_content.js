851e558e06add042642033091d08f28f8afb3cbb
Liwei Xia <liwei.xia0223@gmail.com>
2018-08-23 16:18:04 +0800

    解决跨域访问的问题，同时解决右键时的移动BUG

--- js/translate_content.js
+++ js/translate_content.js
@@ -1,5 +1,7 @@
 console.log("this is translate_content.js")
-$("body").append("<div id='translate_content_card'>123</div>")
+$(window).on("load",function (e) {
+    $("body").append("<div id='translate_content_card'>123</div>")
+})
 translate_content_card_move = false
 translate_content_card_move_x = 0
 translate_content_card_move_y = 0
@@ -18,18 +20,22 @@ $("body").on("mousedown","#translate_content_card",function(e){
 })
 $(window).on("mousemove",function(e){
     if (translate_content_card_move){
-        e.preventDefault()
-        mouse_move_x = e.clientX
-        mouse_move_y = e.clientY
-        move_value_x = mouse_move_x - mouse_x
-        move_value_y = mouse_move_y - mouse_y
-        console.log(move_value_x,move_value_y)
-        $("#translate_content_card").css(
-            {
-                "left":(translate_content_card_move_x+move_value_x)+"px",
-                "top":(translate_content_card_move_y+move_value_y)+"px"
-            }
-        )
+        if(e.which){
+            e.preventDefault()
+            e.stopPropagation()
+            mouse_move_x = e.clientX
+            mouse_move_y = e.clientY
+            move_value_x = mouse_move_x - mouse_x
+            move_value_y = mouse_move_y - mouse_y
+            $("#translate_content_card").css(
+                {
+                    "left":(translate_content_card_move_x+move_value_x)+"px",
+                    "top":(translate_content_card_move_y+move_value_y)+"px"
+                }
+            )
+        }else{
+            translate_content_card_move = false
+        }
     }
 })
 $(window).on("mouseup",function(e){
