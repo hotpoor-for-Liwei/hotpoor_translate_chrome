41154083730c2e2260d110a1f2fd0c7c36eedf99
Liwei Xia <liwei.xia0223@gmail.com>
2018-08-25 11:20:36 +0800

    放到居右位置更好，同时划线时需要额外点击，依旧有BUG。

--- js/translate_content.js
+++ js/translate_content.js
@@ -3,7 +3,7 @@
   console.log("this is translate_demo.js");
 
   $(function() {
-    var getWord, hotpoor_translate, hotpoor_translate_save, mouse_move_x, mouse_move_y, mouse_x, mouse_y, translate_content_card_move, translate_content_card_move_x, translate_content_card_move_y;
+    var getWord, hotpoor_translate, hotpoor_translate_save, latest_translate_content, mouse_move_x, mouse_move_y, mouse_x, mouse_y, translate_content_card_control, translate_content_card_move, translate_content_card_move_x, translate_content_card_move_y;
     hotpoor_translate_save = null;
     hotpoor_translate = function(content, fromLan, toLan) {
       clearTimeout(hotpoor_translate_save);
@@ -31,9 +31,10 @@
                 content_results = result_i["trans_result"];
                 for (j = 0, len1 = content_results.length; j < len1; j++) {
                   result = content_results[j];
-                  content_result = `${content_result}${result["dst"]}\n`;
+                  content_result = `${content_result}${result["dst"]}<br>`;
                 }
-                $("#translate_content_card_content_aim").val(content_result);
+                $("#translate_content_card_content_aim").html(content_result);
+                $("#translate_content_card_move_count").html(`${content_result.length}/1000`);
               }
             }
             return $("#translate_content_card_move_title").text("翻译·Translate");
@@ -45,7 +46,8 @@
         });
       }, 300);
     };
-    $("body").append("<div id=\"translate_content_card\">\n    <div id=\"translate_content_card_move\">\n        <img src=\"https://www.hotpoor.com/static/img/translate.png\"><span id=\"translate_content_card_move_title\">翻译·Translate</span>\n    </div>\n    <div id=\"translate_content_card_tools\" style=\"display:none\"></div>\n    <div id=\"translate_content_card_content\">\n        <textarea id=\"translate_content_card_content_aim\"></textarea>\n    </div>\n</div>");
+    $("body").append("<div id=\"translate_content_card\">\n    <div id=\"translate_content_card_move\">\n        <img src=\"https://www.hotpoor.com/static/img/translate.png\">\n        <span id=\"translate_content_card_move_title\">翻译·Translate</span>\n        <span id=\"translate_content_card_move_count\">0/1000</span>\n    </div>\n    <div id=\"translate_content_card_tools\" style=\"display:none\"></div>\n    <div id=\"translate_content_card_content\">\n        <div id=\"translate_content_card_content_aim\" contenteditable=\"true\"></div>\n    </div>\n</div>");
+    translate_content_card_control = false;
     translate_content_card_move = false;
     translate_content_card_move_x = 0;
     translate_content_card_move_y = 0;
@@ -57,10 +59,20 @@
       e.stopPropagation();
       e.preventDefault();
       translate_content_card_move = true;
-      translate_content_card_move_x = parseInt($("#translate_content_card").css("left"));
+      // translate_content_card_move_x = parseInt $("#translate_content_card").css("left")
+      // translate_content_card_move_y = parseInt $("#translate_content_card").css("top")
+      translate_content_card_move_x = parseInt($("#translate_content_card").css("right"));
       translate_content_card_move_y = parseInt($("#translate_content_card").css("top"));
       mouse_x = e.clientX;
       return mouse_y = e.clientY;
+    });
+    $("body").on("mouseup", "#translate_content_card", function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      return translate_content_card_control = true;
+    });
+    $("body").on("mousedown", "#translate_content_card", function(e) {
+      return translate_content_card_control = true;
     });
     $(window).on("mousemove", function(e) {
       var move_value_x, move_value_y;
@@ -73,7 +85,9 @@
           move_value_x = mouse_move_x - mouse_x;
           move_value_y = mouse_move_y - mouse_y;
           return $("#translate_content_card").css({
-            "left": (translate_content_card_move_x + move_value_x) + "px",
+            // "left":(translate_content_card_move_x+move_value_x)+"px",
+            // "top":(translate_content_card_move_y+move_value_y)+"px",
+            "right": (translate_content_card_move_x - move_value_x) + "px",
             "top": (translate_content_card_move_y + move_value_y) + "px"
           });
         } else {
@@ -86,13 +100,29 @@
       word = window.getSelection ? window.getSelection() : document.selection.createRange().text;
       return word;
     };
+    latest_translate_content = null;
     return $(window).on("mouseup", function(e) {
-      var translate_content;
-      translate_content_card_move = false;
-      translate_content = getWord().toString();
-      if (translate_content.length > 0) {
-        return hotpoor_translate(translate_content, "en", "zh");
+      var translate_content, translate_content_length;
+      if (!translate_content_card_control) {
+        translate_content = getWord().toString();
+        console.log("=== 1");
+        console.log(translate_content);
+        console.log("=== 2");
+        if ((translate_content === latest_translate_content) || (translate_content === "")) {
+          return;
+        }
+        $("#translate_content_card_move_count").html("0/1000");
+        translate_content_length = translate_content.length;
+        if (translate_content_length <= 2000) {
+          hotpoor_translate(translate_content, "en", "zh");
+          latest_translate_content = translate_content;
+        } else if (translate_content_length > 2000) {
+          $("#translate_content_card_content_aim").html("大哥大姐，内容太多翻得扛不住哟。<br>少一点吧~");
+          $("#translate_content_card_move_count").html(`${translate_content_length}/1000`);
+        }
       }
+      translate_content_card_control = false;
+      return translate_content_card_move = false;
     });
   });
 
